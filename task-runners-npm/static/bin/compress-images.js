d4660df2278b2eba320ddd78da5f31ffe6ff1884
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2020-08-19 03:39:32 +0200

    TASK RUNNERS NPM. Tested the 'npm run static:img:compressed' with tinify png compression.
    This option works on Mac, Windows, and Ubuntu, but requires a key and is limited to 500 compressions per month.
    You only pay for what you use. The first 500 compressions each month are free. You will only be billed if you compress more than 500 images.
    Despite this, this is the best option, since it compiles in all operating systems the first time and without headaches due to incompatibility of npm packages.
    https://tinypng.com/developers
    https://tinypng.com/dashboard/api

--- task-runners-npm/static/bin/compress-images.js
+++ task-runners-npm/static/bin/compress-images.js
@@ -46,7 +46,7 @@ function compressImages(inputPath, outputPath) {
 			onProgress,
 			enginesSetup: {
 				jpg: { engine: 'mozjpeg', command: ['-quality', '60'] },
-				png: { engine: 'pngquant', command: ['--quality=20-50'] },
+				png: { engine: 'tinify', key: 'XXXXXXXXX', command: false },
 				svg: { engine: 'svgo', command: ['--multipass'] },
 				gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] },
 			}
